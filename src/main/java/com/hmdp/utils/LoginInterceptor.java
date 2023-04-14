079919a88afbe6a4e3a55da38252d1c15816f28f
Heartbeat-Lin <1810689947@qq.com>
2023-04-14 13:59:08 +0800

    4-14:登录、验证码发送、me功能实现

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,12 +1,17 @@
 package com.hmdp.utils;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
 
@@ -28,9 +33,27 @@ public class LoginInterceptor implements HandlerInterceptor {
             return false;
         }
 
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user,userDTO);
+        log.debug("userDTO:{}",JSONUtil.toJsonStr(userDTO));
+
         //4.存在就保存到ThreadLocal，并返回true
+        UserHolder.saveUser(userDTO);
 
+        return true;
+    }
 
-        return false;
+    public static UserDTO castUserToDTO(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setNickName(user.getNickName());
+        if (user.getIcon()!=null)userDTO.setIcon(user.getIcon());
+        return userDTO;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
     }
 }
